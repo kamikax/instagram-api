63353894326a0c1e68dd4ea13fd0b90859bd0ab1
kamikax <kamikax@gmail.com>
2024-05-13 17:20:09 +0900

    Update autoload_psr4.php

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,5 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
-    'SuphiGram\\Instagram\\' => array($baseDir . '/src'),
+    'Kamikax\\Instagram\\' => array($baseDir . '/src'),
 );
